22cfc5f4e4ab42694926b8b276631d9d04a7534a
fishstack1142 <fishstack1142@gmail.com>
2020-08-08 02:36:40 +0700

    Add graphql with apollo

---
+++ src/index.js
@@ -0,0 +1,29 @@
+
+import express from 'express';
+import server from './server'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const { DB_USER, DB_PASSWORD, DB_NAME } = process.env
+const PORT = 4444;
+
+const createServer = async () => {
+  try {
+
+    await mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@graphql-basic.pmnob.gcp.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`,
+    { useUnifiedTopology: true })
+  
+    const app = express()
+  
+    server.applyMiddleware({ app });
+    
+    app.listen({ port: PORT }, () =>
+      console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
+    )
+  } catch(error) {
+    console.log(error)
+  }
+}
+
+createServer()
